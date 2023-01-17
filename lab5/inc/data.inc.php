cc0198e0661b39c1216547470fca334a1d4ba349
Captoe1 <77244077+Captoe1@users.noreply.github.com>
2023-01-17 16:33:18 +0600

    Add files via upload

---
+++ lab5/inc/data.inc.php
@@ -0,0 +1,24 @@
+<?php
+  /*
+   * Получаем текущий час в виде строки от 00 до 23
+   * и приводим строку к целому числу от 0 до 23
+   */
+  $hour = (int) strftime('%H');
+  $welcome = ''; // Инициализируем переменную для приветствия
+  // Установка локали и выбор значений даты
+  setlocale(LC_ALL, "russian");
+  $day = strftime('%d');
+  $mon = strftime('%m');
+  $year = strftime('%Y');
+  if ($hour >= 0& $hour < 6) {
+    $welcome = 'Доброй ночи';
+  } elseif ($hour >= 6& $hour < 12) {
+    $welcome = 'Доброе утро';
+  } elseif ($hour >= 12& $hour < 18) {
+    $welcome = 'Добрый день';
+  } elseif ($hour >= 18& $hour < 23) {
+    $welcome = 'Добрый вечер';
+  } else {
+    $welcome = 'Доброй ночи';
+  }
+  ?>
---
+++ lab5/table.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Таблица умножения</title>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+  <?php
+  include "inc\lib.inc.php";
+  ?>
+  <div id="header">
+    <!-- Верхняя часть страницы -->
+    <img src="logo.gif" width="187" height="29" alt="Наш логотип" class="logo" />
+    <span class="slogan">приходите к нам учиться</span>
+    <!-- Верхняя часть страницы -->
+  </div>
+
+  <div id="content">
+    <!-- Заголовок -->
+    <h1>Таблица умножения</h1>
+    <!-- Заголовок -->
+    <!-- Область основного контента -->
+    <form action=''>
+      <label>Количество колонок: </label>
+      <br />
+      <input name='cols' type='text' value="" />
+      <br />
+      <label>Количество строк: </label>
+      <br />
+      <input name='rows' type='text' value="" />
+      <br />
+      <label>Цвет: </label>
+      <br />
+      <input name='color' type='text' value="" />
+      <br />
+      <br />
+      <input type='submit' value='Создать' />
+    </form>
+    <!-- Таблица -->
+    <?php
+    drawTable(7, 6, "red");
+    ?>
+    <!-- Таблица -->
+    <!-- Область основного контента -->
+  </div>
+  <div id="nav">
+    <h2>Навигация по сайту</h2>
+    <!-- Меню -->
+    <?php
+    $leftmenu = [
+      ['link' => 'Домой', 'href' => 'index.php'],
+      ['link' => 'О нас', 'href' => 'about.php'],
+      ['link' => 'Контакты', 'href' => 'contact.php'],
+      ['link' => 'Таблица умножения', 'href' => 'table.php'],
+      ['link' => 'Калькулятор', 'href' => 'calc.php']
+    ];
+    drawMenu($leftmenu,false);
+    ?>
+    <!-- Меню -->
+  </div>
+  <div id="footer">
+    <!-- Нижняя часть страницы -->
+    &copy; Супер Мега Веб-мастер, 2000 &ndash; 2021
+    <!-- Нижняя часть страницы -->
+  </div>
+</body>
+
+</html>
